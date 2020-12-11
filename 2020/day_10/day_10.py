4d513dfe3057532402be3a120c5239da9e4a7767
Ricardo Busquet <7198302+rbusquet@users.noreply.github.com>
2020-12-10 22:59:07 -0500

    Day 10, super proud

---
+++ 2020/day_10/day_10.py
@@ -0,0 +1,50 @@
+from collections import defaultdict, deque, Counter
+from functools import lru_cache
+
+def read_file():
+    with open("./input.txt") as f:
+        yield from map(lambda c: c.strip(), f.readlines())
+
+
+adapters = set(map(int, read_file()))
+
+diffs = defaultdict(int)
+
+current_joltage = 0
+while adapters:
+    for diff in range(1, 4):  # [1,2,3]
+        need_joltage = current_joltage + diff
+        if need_joltage in adapters:
+            current_joltage = need_joltage
+            diffs[diff] += 1
+            adapters.remove(need_joltage)
+            break
+
+one_diff = diffs[1]
+three_diff = diffs[3] + 1
+print("--- part 1 ---")
+print(one_diff * three_diff)
+
+
+adapters = set(map(int, read_file()))
+max_voltage = max(adapters) + 3
+adapters.add(0)
+adapters.add(max_voltage)
+
+current_joltage = 0
+
+# paths[n] is the total paths from 0 to n
+paths = defaultdict(int)
+paths[0] = 1
+for adapter in sorted(adapters):
+    for diff in range(1, 4):
+        next_adapter = adapter + diff
+        if next_adapter in adapters:
+            paths[next_adapter] += paths[adapter]
+
+print("--- part 2 ---")
+print(paths[max_voltage])
+
+
+
+
