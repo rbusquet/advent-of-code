84adf70977f880c8acc2fe826ce0ca6e893ca841
Ricardo Busquet <7198302+rbusquet@users.noreply.github.com>
2020-12-13 01:30:44 -0500

    add programatic solution

--- 2020/day_13/day_13.py
+++ 2020/day_13/day_13.py
@@ -24,7 +24,22 @@ service = enumerate(next(f).split(','))
 valid = [*filter(lambda x: x[1] != 'x', service)]
 print(valid)
 
+## this is cheating
 for i, bus in valid:
     print(f"(t + {i}) mod {bus} = 0;")
 # :eyes: https://www.wolframalpha.com/input/?i=solve+%28t+%2B+0%29+mod+29+%3D+0%3B+%28t+%2B+23%29+mod+37+%3D+0%3B+%28t+%2B+29%29+mod+631+%3D+0%3B+%28t+%2B+47%29+mod+13+%3D+0%3B+%28t+%2B+48%29+mod+19+%3D+0%3B+%28t+%2B+52%29+mod+23+%3D+0%3B+%28t+%2B+60%29+mod+383+%3D+0%3B+%28t+%2B+70%29+mod+41+%3D+0%3B+%28t+%2B+77%29+mod+17+%3D+0%3B
 print(894954360381385)
+
+# for real now
+ts = 0
+step = int(valid[0][1])
+
+for offset, time in valid[1:]:
+    time = int(time)
+    while True:
+        ts += step
+        if (ts + offset) % time == 0:
+            # found the first match
+            break
+    step *= time
+print(ts)
