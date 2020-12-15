97a80aa454f99452f6c0f8d2172ec2bea0c5011d
Ricardo Busquet <7198302+rbusquet@users.noreply.github.com>
2020-12-15 10:54:49 -0500

    day 15

---
+++ 2020/day_15/day_15.py
@@ -0,0 +1,30 @@
+from progress.bar import PixelBar
+
+def memory_game(initial, stop):
+    seen = dict()
+    index = 0
+    for value in initial[:-1]:
+        seen[value] = index
+        index += 1
+        # print(f"last spoken: {value}")
+
+    last_spoken = initial[-1]
+    # print(f"last spoken: {last_spoken}")
+    with PixelBar() as bar:
+        for index in range(index, stop - 1):
+            next_spoken = index - seen.get(last_spoken, index)
+            seen[last_spoken] = index
+            last_spoken = next_spoken
+            if index % ((stop - 1) // 100) == 0:
+                bar.next()
+
+    return last_spoken
+
+print("--- part 1 ---")
+print(memory_game([14, 3, 1, 0, 9, 5], 2020))
+print("--- part 2 ---")
+print(memory_game([14, 3, 1, 0, 9, 5], 30_000_000))
+
+
+
+
