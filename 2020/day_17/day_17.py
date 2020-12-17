8b3a94b8dfa0f1c47dda8f6707597978b1610345
Ricardo Busquet <7198302+rbusquet@users.noreply.github.com>
2020-12-17 11:33:49 -0500

    major optmization :fire:

--- 2020/day_17/day_17.py
+++ 2020/day_17/day_17.py
@@ -12,76 +12,48 @@ initial = """
 #...##..
 """.strip()
 
-def neighbors(*position):
+
+def neighborhood(*position):
     for diff in product([-1, 0, 1], repeat=len(position)):
         neighbor = tuple(pos + diff[i] for i, pos in enumerate(position))
-        if neighbor == position:
-            continue
         yield neighbor
 
-    #     active_count += layout[neighbor] == '#'
-    # if layout[position] == '#':
-    #     if active_count not in [2, 3]:
-    #         return '.'
-    # else:
-    #     if active_count == 3:
-    #         return '#'
-    # return layout[position]
+
+def full_cycle(initial, dimensions):
+    space = defaultdict(lambda: ".")
+    padding = (0,) * (dimensions - 2)
+    for x, line in enumerate(initial.splitlines()):
+        for y, state in enumerate(line):
+            cube = (x, y) + padding
+            space[cube] = state
+
+    for _ in range(6):
+        cube_to_active_count = defaultdict(int)
+
+        for cube in space:
+            if space[cube] == ".":
+                continue
+            for n in neighborhood(*cube):
+                # neighborhood contains cube and all its neighbors.
+                # `cube_to_active_count[n] += n != cube` ensures
+                # active cubes without active neighbors are counted
+                # and proper deactivated by underpopulation in the
+                # next for-loop.
+                cube_to_active_count[n] += n != cube and space[cube] == "#"
+        for n, count in cube_to_active_count.items():
+            if space[n] == "#":
+                if count in [2, 3]:
+                    space[n] = "#"
+                else:
+                    space[n] = "."
+            elif space[n] == ".":
+                if count == 3:
+                    space[n] = "#"
+
+    return sum(state == "#" for state in space.values())
 
 
-space = defaultdict(lambda: '.')
-for x, line in enumerate(initial.splitlines()):
-    for y, state in enumerate(line):
-        cube = x, y, 0
-        space[cube] = state
-
-def count_active(space):
-    return len([1 for state in space.values() if state == '#'])
-
-print(count_active(space))
-for _ in range(6):
-    cube_to_active_count = defaultdict(int)
-
-    for cube in space:
-        for n in neighbors(*cube):
-            cube_to_active_count[n] += space[cube] == '#'
-    for n, count in cube_to_active_count.items():
-        if space[n] == '#':
-            if count in [2, 3]:
-                space[n] = '#'
-            else:
-                space[n] = '.'
-        elif space[n] == '.':
-            if count == 3:
-                space[n] = '#'
-
-    print(count_active(space))
-
-
-space = defaultdict(lambda: '.')
-for x, line in enumerate(initial.splitlines()):
-    for y, state in enumerate(line):
-        cube = x, y, 0, 0
-        space[cube] = state
-
-def count_active(space):
-    return len([1 for state in space.values() if state == '#'])
-
-print(count_active(space))
-for _ in range(6):
-    cube_to_active_count = defaultdict(int)
-
-    for cube in space:
-        for n in neighbors(*cube):
-            cube_to_active_count[n] += space[cube] == '#'
-    for n, count in cube_to_active_count.items():
-        if space[n] == '#':
-            if count in [2, 3]:
-                space[n] = '#'
-            else:
-                space[n] = '.'
-        elif space[n] == '.':
-            if count == 3:
-                space[n] = '#'
-
-    print(count_active(space))
+print("--- part 1 ---")
+print(full_cycle(initial, 3))
+print("--- part 2 ---")
+print(full_cycle(initial, 4))
