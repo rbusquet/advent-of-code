796510b0cf5ea0f48e76bba806c68d817db7dd30
Ricardo Busquet <7198302+rbusquet@users.noreply.github.com>
2020-12-21 12:36:02 -0500

    day 21

---
+++ 2020/day_21/day_21.py
@@ -0,0 +1,60 @@
+from collections import defaultdict
+from functools import reduce
+from operator import and_
+import re
+
+
+def read_file():
+    with open("./input.txt") as f:
+        yield from map(lambda c: c.strip(), f.readlines())
+
+
+allergen_to_recipes = defaultdict(list)
+
+food_re = re.compile(r"(.*)\(contains([\s\w,]*)\)")
+
+all_foods = []
+
+for line in read_file():
+    food, allergens = food_re.match(line).groups()
+    ingredients = food.strip().split(" ")
+    all_foods.append(ingredients)
+
+    allergens = [a.strip() for a in allergens.split(",")]
+
+    for allergen in allergens:
+        allergen_to_recipes[allergen].append(set(ingredients))
+
+
+dangerous = set()
+
+for allergen, foods in allergen_to_recipes.items():
+    appears_in_all = reduce(and_, foods)
+    dangerous |= appears_in_all
+
+count = 0
+for food in all_foods:
+    for ingredient in food:
+        if ingredient not in dangerous:
+            count += 1
+print(count)
+
+matched_allergens = {}
+
+while len(matched_allergens) < len(dangerous):
+    dangerous_to_allergen = defaultdict(set)
+    for allergen, foods in allergen_to_recipes.items():
+        if allergen in matched_allergens.values():
+            continue
+        appears_in_all = reduce(and_, foods)
+        for danger in appears_in_all:
+            dangerous_to_allergen[danger].add(allergen)
+    for food, allergens in dangerous_to_allergen.items():
+        if len(allergens) == 1:
+            matched_allergens[food] = allergens.pop()
+            break
+
+cannonical_list = ",".join(
+    sorted(matched_allergens.keys(), key=lambda food: matched_allergens[food])
+)
+print(cannonical_list)
