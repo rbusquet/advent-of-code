9f90d6235fb0284ea19187d9117303074ec54d72
Ricardo Busquet <7198302+rbusquet@users.noreply.github.com>
2021-12-01 09:02:39 -0500

    initialize all days

---
+++ 2021/03/day3.py
@@ -0,0 +1,11 @@
+from pathlib import Path
+
+
+def part_1():
+    with open(Path(__file__).parent / "input.txt") as file:
+        pass
+
+
+def part_2():
+    with open(Path(__file__).parent / "input.txt") as file:
+        pass
---
+++ 2021/initialize.py
@@ -0,0 +1,27 @@
+from pathlib import Path
+
+
+DAY_TEMPLATE = """from pathlib import Path
+
+
+def part_1():
+    with open(Path(__file__).parent / "input.txt") as file:
+        pass
+
+
+def part_2():
+    with open(Path(__file__).parent / "input.txt") as file:
+        pass
+"""
+
+
+for i in range(2, 26):
+    dir = Path(f"{i:02}")
+    file = dir / f"day{i}.py"
+    input_file = dir / "input.txt"
+    dir.mkdir(exist_ok=True)
+    file.touch(exist_ok=True)
+    input_file.touch(exist_ok=True)
+
+    with open(file, "w") as mod:
+        mod.write(DAY_TEMPLATE)
