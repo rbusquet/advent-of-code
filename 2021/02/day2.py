35ecbd931fc3b74bf702fdb7b7f9901533f72c29
Ricardo Busquet <7198302+rbusquet@users.noreply.github.com>
2021-12-02 07:53:29 -0500

    day 2

--- 2021/02/day2.py
+++ 2021/02/day2.py
@@ -3,9 +3,32 @@ from pathlib import Path
 
 def part_1():
     with open(Path(__file__).parent / "input.txt") as file:  # noqa: F841
-        pass
+        horizontal = depth = 0
+        for command in file:
+            match command.split(" "):
+                case ["forward", distance]:
+                    horizontal += int(distance)
+                case ["up", distance]:
+                    depth -= int(distance)
+                case ["down", distance]:
+                    depth += int(distance)
+        return horizontal * depth
 
 
 def part_2():
     with open(Path(__file__).parent / "input.txt") as file:  # noqa: F841
-        pass
+        horizontal = depth = aim = 0
+        for command in file:
+            match command.split(" "):
+                case ["forward", distance]:
+                    horizontal += int(distance)
+                    depth += int(distance) * aim
+                case ["up", distance]:
+                    aim -= int(distance)
+                case ["down", distance]:
+                    aim += int(distance)
+        return horizontal * depth
+
+
+print(part_1())  # 1604850
+print(part_2())
