71310ad8b4b5a85ca7a828ab9803417982e1586a
Ricardo Busquet <7198302+rbusquet@users.noreply.github.com>
2021-12-06 18:09:13 -0500

    playing around

--- 2021/06/day6.py
+++ 2021/06/day6.py
@@ -1,10 +1,29 @@
+import time
 from collections import Counter, defaultdict
+from functools import wraps
+from heapq import heapify, heappop, heappush
 from pathlib import Path
 
 DAYS = 80
 
 
+def timeit(fn):
+    @wraps(fn)
+    def wrapper(*args, **kwargs):
+        t0 = time.perf_counter_ns()
+        ret = fn(*args, **kwargs)
+        t1 = time.perf_counter_ns()
+        print(f"{fn.__name__} ran in {t1 - t0}ns")
+        return ret
+
+    return wrapper
+
+
+@timeit
 def grow_slow(fishes: list[int], max_days: int) -> int:
+    """
+    Naive implementation, solves for part 1 but can't do part 2.
+    """
     days = 0
     while days < max_days:
         new_fishes = []
@@ -21,7 +40,11 @@ def grow_slow(fishes: list[int], max_days: int) -> int:
     return len(fishes)
 
 
+@timeit
 def grow_fast(initial: list[int], max_days: int) -> int:
+    """
+    Got the right answer with this solution.
+    """
     same_age = Counter(initial)
     fishes_by_age = same_age.most_common()
     days = 0
@@ -38,7 +61,55 @@ def grow_fast(initial: list[int], max_days: int) -> int:
             if week_ended:
                 next_fishes[8] += total_fishes
         fishes_by_age = list(next_fishes.items())
-        # print(fishes_by_age)
+
+        days += 1
+    return sum(f[1] for f in fishes_by_age)
+
+
+@timeit
+def grow_faster(initial: list[int], max_days: int) -> int:
+    """
+    Similar solution to above, but avoids creating new lists.
+    Takes about half the time as the above.
+    """
+    same_age = Counter(initial)
+    days = 0
+
+    while days < max_days:
+        spawning = same_age.pop(0, 0)
+        for age in range(8):
+            same_age[age] = same_age[age + 1]
+        same_age[6] += spawning
+        same_age[8] = spawning
+
+        days += 1
+    return sum(same_age.values())
+
+
+@timeit
+def grow_fastest(initial: list[int], max_days: int) -> int:
+    """
+    Fastest implementation by using a heapq and just popping items, not shifting.
+
+    The real fastes solution uses algebra / matrices I don't know how to do that
+    """
+    same_age = Counter(initial)
+    fishes_by_age = same_age.most_common()
+
+    heapify(fishes_by_age)
+    days = 0
+
+    while days < max_days:
+        day_spawn = 0
+        while fishes_by_age[0][0] == days:
+            _, spawn = heappop(fishes_by_age)
+            day_spawn += spawn
+
+        seven = days + 7
+        nine = days + 9
+
+        heappush(fishes_by_age, (seven, day_spawn))
+        heappush(fishes_by_age, (nine, day_spawn))
 
         days += 1
     return sum(f[1] for f in fishes_by_age)
@@ -56,6 +127,20 @@ def part_2() -> int:
     return grow_fast(fishes, 256)
 
 
+def part_2_faster() -> int:
+    with open(Path(__file__).parent / "input.txt") as file:  # noqa: F841
+        fishes = list(map(int, file.readline().split(",")))
+    return grow_faster(fishes, 256)
+
+
+def part_2_fastest() -> int:
+    with open(Path(__file__).parent / "input.txt") as file:  # noqa: F841
+        fishes = list(map(int, file.readline().split(",")))
+    return grow_fastest(fishes, 256)
+
+
 if __name__ == "__main__":
     print(part_1())
     print(part_2())
+    print(part_2_faster())
+    print(part_2_fastest())
