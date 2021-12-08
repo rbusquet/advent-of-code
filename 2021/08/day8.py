769387862e6c75dd423f0e9b4402577ff090790b
Ricardo Busquet <7198302+rbusquet@users.noreply.github.com>
2021-12-08 09:16:19 -0500

    day 8 part 1

--- 2021/08/day8.py
+++ 2021/08/day8.py
@@ -1,14 +1,51 @@
 from pathlib import Path
 
+CORRECT_DISPLAY_MAP = dict(
+    zip(
+        [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+        [
+            0b1110111,
+            0b0010010,
+            0b1011101,
+            0b1011011,
+            0b0111010,
+            0b1101011,
+            0b1101111,
+            0b1010010,
+            0b1111111,
+            0b1111011,
+        ],
+    )
+)
 
-def part_1():
+EASY_DIGITS_LENGTHS = [2, 4, 3, 7]
+EASY_DIGITS = [1, 4, 7, 8]
+EASY_DECODER = dict(zip(EASY_DIGITS_LENGTHS, EASY_DIGITS))
+
+
+def part_1() -> int:
+    with open(Path(__file__).parent / "input.txt") as file:
+        count = 0
+        for line in file:
+            _, outputs = line.strip().split(" | ")
+            for digit in outputs.split(" "):
+                count += len(digit) in EASY_DIGITS_LENGTHS
+
+    return count
+
+
+def part_2() -> int:
     with open(Path(__file__).parent / "input.txt") as file:  # noqa: F841
-        pass
 
+        for line in file:
+            decoded: list[int] = []
+            _, outputs = line.strip().split(" | ")
+            for digit in outputs.split(" "):
+                if len(digit) in EASY_DIGITS_LENGTHS:
+                    decoded.append(EASY_DECODER[len(digit)])
+                    continue
 
-def part_2():
-    with open(Path(__file__).parent / "input.txt") as file:  # noqa: F841
-        pass
+    return 0
 
 
 if __name__ == "__main__":
