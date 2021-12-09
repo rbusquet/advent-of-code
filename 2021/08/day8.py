f680dc4bfc4522313de1cb0d9f2d7e7573ee34fc
Ricardo Busquet <7198302+rbusquet@users.noreply.github.com>
2021-12-08 20:43:08 -0500

    day 8 part 2 :(

--- 2021/08/day8.py
+++ 2021/08/day8.py
@@ -4,19 +4,21 @@ CORRECT_DISPLAY_MAP = dict(
     zip(
         [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
         [
-            0b1110111,
-            0b0010010,
-            0b1011101,
-            0b1011011,
-            0b0111010,
-            0b1101011,
-            0b1101111,
-            0b1010010,
-            0b1111111,
-            0b1111011,
+            set("abcefg"),
+            set("cf"),
+            set("acfef"),
+            set("acdfg"),
+            set("bcdf"),
+            set("abdfg"),
+            set("abdefg"),
+            set("acf"),
+            set("abcdefg"),
+            set("abcdfg"),
         ],
     )
 )
+
+DISPLAY_TO_NUMBER = {frozenset(v): k for k, v in CORRECT_DISPLAY_MAP.items()}
 
 EASY_DIGITS_LENGTHS = [2, 4, 3, 7]
 EASY_DIGITS = [1, 4, 7, 8]
@@ -34,18 +36,39 @@ def part_1() -> int:
     return count
 
 
+HARD_DECODER = {5: [2, 3, 5], 6: [0, 6, 9]}
+
+
 def part_2() -> int:
     with open(Path(__file__).parent / "input.txt") as file:  # noqa: F841
-
+        final = 0
         for line in file:
-            decoded: list[int] = []
-            _, outputs = line.strip().split(" | ")
-            for digit in outputs.split(" "):
-                if len(digit) in EASY_DIGITS_LENGTHS:
-                    decoded.append(EASY_DECODER[len(digit)])
-                    continue
+            patterns, outputs = line.strip().split(" | ")
+            sized_patterns = {len(s): set(s) for s in patterns.split()}
+            n = ""
+            for o in outputs.strip().split():
+                output = set(o)
+                checksum = (
+                    len(output),
+                    len(output & sized_patterns[4]),
+                    len(output & sized_patterns[2]),
+                )
+                # fmt: off
+                match checksum:  # noqa: E501
+                    case 2, _, _: n += '1'
+                    case 3, _, _: n += '7'
+                    case 4, _, _: n += '4'
+                    case 7, _, _: n += '8'
+                    case 5, 2, _: n += '2'
+                    case 5, 3, 1: n += '5'
+                    case 5, 3, 2: n += '3'
+                    case 6, 4, _: n += '9'
+                    case 6, 3, 1: n += '6'
+                    case 6, 3, 2: n += "0"
+                # fmt: on
+            final += int(n)
 
-    return 0
+    return final
 
 
 if __name__ == "__main__":
