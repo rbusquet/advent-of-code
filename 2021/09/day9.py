c2bbec9c439d0b8fd71b64978c3ef0f33f5f47a8
Ricardo Busquet <7198302+rbusquet@users.noreply.github.com>
2021-12-09 09:20:42 -0500

    day 9 part2

--- 2021/09/day9.py
+++ 2021/09/day9.py
@@ -1,5 +1,8 @@
+import functools
+from collections import defaultdict, deque
+from heapq import nlargest
 from pathlib import Path
-from typing import TextIO
+from typing import Iterator, TextIO
 
 N = (-1, 0)
 S = (1, 0)
@@ -10,12 +13,15 @@ Map = dict[tuple[int, int], int]
 Point = tuple[int, int]
 
 
-def is_low_point(i: int, j: int, height_map: Map) -> bool:
-    neighbors = []
+def neighborhood(i: int, j: int) -> Iterator[Point]:
     for ii, jj in (N, S, E, W):
-        if (i + ii, j + jj) in height_map:
-            neighbors.append(height_map[i + ii, j + jj])
-    return all(height_map[i, j] < n for n in neighbors)
+        yield i + ii, j + jj
+
+
+def is_low_point(i: int, j: int, height_map: Map) -> bool:
+    return all(
+        height_map[i, j] < height_map.get((ii, jj), 10) for ii, jj in neighborhood(i, j)
+    )
 
 
 def part_1() -> int:
@@ -26,10 +32,11 @@ def part_1() -> int:
 
 
 def generate_low_points(file: TextIO) -> tuple[Map, list[Point]]:
-    height_map = dict[tuple[int, int], int]()
+    height_map = defaultdict[tuple[int, int], int](lambda: 10)
     for i, line in enumerate(file):
         for j, height in enumerate(line.strip()):
             height_map[i, j] = int(height)
+        print("".join(i if i == "9" else "." for i in line))
     low_points = []
     for i, j in height_map:
         if is_low_point(i, j, height_map):
@@ -37,10 +44,24 @@ def generate_low_points(file: TextIO) -> tuple[Map, list[Point]]:
     return height_map, low_points
 
 
-def part_2() -> int:  # type: ignore[return]
+def part_2() -> int:
     with open(Path(__file__).parent / "input.txt") as file:
         height_map, low_points = generate_low_points(file)
-        pass
+
+        basins = list[int]()
+        for i, j in low_points:
+            queue = deque[Point]([(i, j)])
+
+            visited = set[Point]()
+            while queue:
+                current = queue.pop()
+                for n in neighborhood(*current):
+                    ii, jj = n
+                    if n not in visited and height_map[ii, jj] < 9:
+                        queue.appendleft(n)
+                visited.add(current)
+            basins.append(len(visited))
+        return functools.reduce(lambda a, b: a * b, nlargest(3, basins))
 
 
 if __name__ == "__main__":
