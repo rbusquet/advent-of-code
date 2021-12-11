5a62c213744e6622e786fed73db84deeb8e1046a
Ricardo Busquet <7198302+rbusquet@users.noreply.github.com>
2021-12-11 13:21:04 -0500

    more 2015

--- 2015/04/day4.py
+++ 2015/04/day4.py
@@ -1,14 +1,28 @@
+from hashlib import md5
+from itertools import count
 from pathlib import Path
 
 
 def part_1() -> int:
-    with open(Path(__file__).parent / "input.txt") as file:  # noqa: F841
-        pass
+    with open(Path(__file__).parent / "input.txt") as file:
+        secret = file.readline()
+    for number in count():
+        result = md5(f"{secret}{number}".encode())
+        init = result.hexdigest()[:5]
+        if init == "00000":
+            return number
+    return -1
 
 
 def part_2() -> int:
     with open(Path(__file__).parent / "input.txt") as file:  # noqa: F841
-        pass
+        secret = file.readline()
+    for number in count():
+        result = md5(f"{secret}{number}".encode())
+        init = result.hexdigest()[:6]
+        if init == "000000":
+            return number
+    return -1
 
 
 if __name__ == "__main__":
