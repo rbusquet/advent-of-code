1002f8482dccf8e78aece480a09d60743a4543bd
Ricardo Busquet <7198302+rbusquet@users.noreply.github.com>
2021-12-12 02:51:12 -0500

    day 12 part 1

--- 2021/12/day12.py
+++ 2021/12/day12.py
@@ -1,14 +1,42 @@
+from collections import defaultdict, deque
 from pathlib import Path
 
-
-def part_1():
-    with open(Path(__file__).parent / "input.txt") as file:  # noqa: F841
-        pass
+Graph = defaultdict[str, list[str]]
 
 
-def part_2():
-    with open(Path(__file__).parent / "input.txt") as file:  # noqa: F841
-        pass
+def paths(graph: Graph, root: str = "start") -> int:
+    queue = deque([(root, {root})])
+    paths = 0
+    while queue:
+        current, path = queue.pop()
+        if current == "END":
+            paths += 1
+            continue
+        for n in graph[current]:
+            if not n.islower() or n not in path:
+                queue.append((n, path | {n}))
+    return paths
+
+
+def part_1() -> int:
+    with open(Path(__file__).parent / "input.txt") as file:
+        graph = Graph(list)
+        for line in file:
+            src, dst = line.strip().replace("end", "END").split("-")
+            graph[src].append(dst)
+            graph[dst].append(src)
+        return paths(graph)
+
+
+def part_2() -> int:
+    with open(Path(__file__).parent / "input.txt") as file:
+        graph = Graph(list)
+        for line in file:
+            src, dst = line.strip().replace("end", "END").split("-")
+            graph[src].append(dst)
+            graph[dst].append(src)
+        # return paths_reviewed(graph)
+    return 0
 
 
 if __name__ == "__main__":
