9871df1bc723620bdf3e7c501a8bea8ddaa7d00d
Ricardo Busquet <7198302+rbusquet@users.noreply.github.com>
2021-12-13 07:42:17 -0500

    class based

--- 2021/13/day13.py
+++ 2021/13/day13.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 from io import StringIO
 from pathlib import Path
 from typing import NamedTuple
@@ -13,8 +15,59 @@ class Instruction(NamedTuple):
     value: int
 
 
-def parse_file() -> tuple[list[Dot], list[Instruction]]:
-    dots = list[Dot]()
+class Paper(list[Dot]):
+    def fold(self, instruction: Instruction) -> Paper:
+        match instruction:
+            case Instruction("x", x):
+                return self._fold_left(x)
+            case Instruction("y", y):
+                return self._fold_up(y)
+
+    def _fold_up(self, line: int) -> Paper:
+        to_remove = list[Dot]()
+        for dot in self:
+            match dot:
+                case Dot(x, y) if y > line:
+                    to_remove.append(dot)
+                    new_dot = Dot(x, 2 * line - y)
+                    if new_dot not in self:
+                        self.append(new_dot)
+        for dot in to_remove:
+            self.remove(dot)
+        return self
+
+    def _fold_left(self, line: int) -> Paper:
+        to_remove = list[Dot]()
+        for dot in self:
+            match dot:
+                case Dot(x, y) if x > line:
+                    to_remove.append(dot)
+                    new_dot = Dot(2 * line - x, y)
+                    if new_dot not in self:
+                        self.append(new_dot)
+        for dot in to_remove:
+            self.remove(dot)
+        return self
+
+    def render(self) -> str:
+        max_y = max(dot[1] for dot in self)
+        max_x = max(dot[0] for dot in self)
+
+        mapped = {dot: dot for dot in self}
+        io = StringIO()
+        for i in range(max_y + 1):
+            for j in range(max_x + 1):
+                if mapped.get((j, i)):
+                    print("🀫", end="", file=io)
+                else:
+                    print(" ", end="", file=io)
+            print(file=io)
+        io.seek(0)
+        return io.read()
+
+
+def parse_file() -> tuple[Paper, list[Instruction]]:
+    dots = Paper()
     instructions = list[Instruction]()
     with open(Path(__file__).parent / "input.txt") as file:
         while line := next(file).strip():
@@ -27,66 +80,17 @@ def parse_file() -> tuple[list[Dot], list[Instruction]]:
     return dots, instructions
 
 
-def fold(paper: list[Dot], instruction: Instruction):
-    match instruction:
-        case Instruction("x", x):
-            return fold_left(paper, x)
-        case Instruction("y", y):
-            return fold_up(paper, y)
-
-
-def fold_up(paper: list[Dot], line: int) -> list[Dot]:
-    folded = set[Dot]()
-
-    for dot in paper:
-        match dot:
-            case Dot(x, y) if y > line:
-                folded.add(Dot(x, 2 * line - y))
-            case _:
-                folded.add(dot)
-    return list(folded)
-
-
-def fold_left(paper: list[Dot], line: int) -> list[Dot]:
-    folded = set[Dot]()
-
-    for dot in paper:
-        match dot:
-            case Dot(x, y) if x > line:
-                folded.add(Dot(2 * line - x, y))
-            case _:
-                folded.add(dot)
-    return list(folded)
-
-
-def render(paper: list[Dot]) -> str:
-    max_y = max(dot[1] for dot in paper)
-    max_x = max(dot[0] for dot in paper)
-
-    mapped = {dot: dot for dot in paper}
-    io = StringIO()
-    for i in range(max_y + 1):
-        for j in range(max_x + 1):
-            if mapped.get((j, i)):
-                print("🀫", end="", file=io)
-            else:
-                print(" ", end="", file=io)
-        print(file=io)
-    io.seek(0)
-    return io.read()
-
-
 def part_1() -> int:
     dots, instructions = parse_file()
-    return len(fold(dots, instructions[0]))
+    return len(dots.fold(instructions[0]))
 
 
 def part_2() -> str:
     dots, instructions = parse_file()
 
     for instruction in instructions:
-        dots = fold(dots, instruction)
-    return render(dots)
+        dots.fold(instruction)
+    return dots.render()
 
 
 if __name__ == "__main__":
