cfbf0969b2b114f1cbdfa960cd27ff2f81fa84ae
Ricardo Busquet <7198302+rbusquet@users.noreply.github.com>
2021-12-20 12:15:30 -0500

    day 20 + wip 19

--- 2021/19/day19.py
+++ 2021/19/day19.py
@@ -1,10 +1,12 @@
 from pathlib import Path
-import numpy as np
-import numpy.typing as npt
 
 from itertools import combinations
+from pprint import pprint
+import more_itertools
 
-Scanners = dict[int, list[npt.NDArray[np.int_]]]
+Vector = tuple[int, int, int]
+
+Scanners = dict[int, list[Vector]]
 
 
 def parse_scanners() -> Scanners:
@@ -17,36 +19,66 @@ def parse_scanners() -> Scanners:
                 scanner_id = int(data[2])
                 scanners[int(scanner_id)] = []
             elif len(data) == 1:
-                beacon = np.array(tuple(map(int, data[0].split(","))))
-                scanners[scanner_id].append(beacon)
+                x, y, z = tuple(map(int, data[0].split(",")))
+                scanners[scanner_id].append((x, y, z))
     return scanners
 
 
-# def part_2():
-#     with open(Path(__file__).parent / "input.txt") as file:  # noqa: F841
-#         pass
+def distance(a: Vector, b: Vector) -> float:
+    x1, y1, z1 = a
+    x2, y2, z2 = b
+    return ((x2 - x1) ** 2 + (y2 - y1) ** 2 + (z2 - z1) ** 2) ** 0.5
 
 
 if __name__ == "__main__":
     scanners = parse_scanners()
+    print(f"{len(scanners)=}")
 
-    distances = set()  # type: ignore
+    distances = dict[float, tuple[Vector, Vector]]()
 
     base_beacons = scanners[0]
+    known_scanners = {0}
+    known_beacons = set(base_beacons)
 
     for a, b in combinations(base_beacons, 2):
-        distances.add(tuple(a - b))
+        if distance(a, b) in distances:
+            print("not unique distance :/")
+        distances[distance(a, b)] = (a, b)
 
-    print(distances)
-    for scanner, beacons in scanners.items():
-        if scanner == 0:
-            continue
-        s_distances = set()
-        for a, b in combinations(beacons, 2):
-            s_distances.add(tuple(a - b))
-        # TODO: rotate all vectors
-        # compare with distances set
-        # if 12 are a match, we found the transformation of this scanner
-        # to scanner 0
-        # add all distances to the base distances
-        # result is the "reverse combination" of the distances.
+    while len(known_scanners) != len(scanners):
+        for scanner, beacons in scanners.items():
+            if scanner in known_scanners:
+                continue
+            current_distances = dict[float, tuple[Vector, Vector]]()
+
+            sample_distance = 0.0
+            seen_vectors = dict[Vector, set[Vector]]()
+            for a, b in combinations(beacons, 2):
+                d = distance(a, b)
+                if distances.get(d):
+                    aa, bb = distances[d]
+                    if a in seen_vectors:
+                        seen_vectors[a] &= {aa, bb}
+                    else:
+                        seen_vectors[a] = {aa, bb}
+                    if b in seen_vectors:
+                        seen_vectors[b] &= {aa, bb}
+                    else:
+                        seen_vectors[b] = {aa, bb}
+
+                else:
+                    current_distances[d] = (a, b)
+            seen_vectors = {k: v for k, v in seen_vectors.items() if v}
+
+            if len(seen_vectors) >= 12:
+                for a, _original in seen_vectors.items():
+                    b = more_itertools.one(_original)
+                    diff = b[0] + a[0], b[1] - a[1], b[2] + a[2]
+                for d, (a, b) in current_distances.items():
+                    aa = (diff[0] - a[0], diff[1] + a[1], diff[2] - a[2])
+                    bb = (diff[0] - b[0], diff[1] + b[1], diff[2] - b[2])
+                    known_beacons.add(aa)
+                    known_beacons.add(bb)
+                    distances[d] = (aa, bb)
+
+    print(len(known_beacons))
--- 2021/20/day20.py
+++ 2021/20/day20.py
@@ -1,14 +1,98 @@
 from pathlib import Path
+from typing import Literal, Sequence, cast
+from collections import defaultdict
+from progress.spinner import LineSpinner
 
 
-def part_1():
+Pixel = tuple[int, int]
+Value = Literal[".", "#"]
+
+POWER_MAP = {
+    (-1, -1): 0,
+    (-1, 0): 1,
+    (-1, 1): 2,
+    (0, -1): 3,
+    (0, 0): 4,
+    (0, 1): 5,
+    (1, -1): 6,
+    (1, 0): 7,
+    (1, 1): 8,
+}
+
+"""
+X X X X X
+X 9 8 7 X  if I'm looking at how a pixel    1 2 3
+X 6 5 4 X ------------------------------->  4 5 6
+X 3 2 1 X      affects its neighboors       7 8 9
+X X X X X
+"""
+
+
+def enhance(
+    known_pixels: dict[Pixel, Value],
+    background: Value,
+    algorithm: Sequence[Value],
+) -> tuple[dict[Pixel, Value], Value]:
+    pixels_to_update = defaultdict[Pixel, int](int)
+
+    for (x1, y1), value in known_pixels.items():
+        for (x2, y2), power in POWER_MAP.items():
+            x, y = x1 + x2, y1 + y2
+            pixels_to_update[x, y] += (value == "#") * pow(2, power)
+
+    result_image = dict[Pixel, Value]()
+    for x1, y1 in pixels_to_update:
+        # for all pixels to update, check if we know all their neighboors
+        for (x2, y2), power in POWER_MAP.items():
+            x, y = x1 + x2, y1 + y2
+            if (x, y) not in known_pixels:
+                revert_effect = 8 - power
+                if background == "#":
+                    pixels_to_update[x1, y1] += pow(2, revert_effect)
+        result_image[x1, y1] = algorithm[pixels_to_update[x1, y1]]
+
+    if background == ".":
+        background = algorithm[0]
+    else:
+        background = algorithm[pow(2, 9) - 1]
+    return result_image, background
+
+
+def part_1() -> int:
+    image = dict[Pixel, Value]()
     with open(Path(__file__).parent / "input.txt") as file:  # noqa: F841
-        pass
+        algorithm = cast(Sequence[Value], file.readline())
+        file.readline()
+
+        for i, line in enumerate(file):
+            for j, char in enumerate(line.strip()):
+                image[i, j] = cast(Value, char)
+
+    image, background = enhance(image, ".", algorithm)
+    image, background = enhance(image, background, algorithm)
+
+    return len([value for value in image.values() if value == "#"])
 
 
-def part_2():
+def part_2() -> int:
+    image = dict[Pixel, Value]()
     with open(Path(__file__).parent / "input.txt") as file:  # noqa: F841
-        pass
+        algorithm = cast(Sequence[Value], file.readline())
+        file.readline()
+
+        for i, line in enumerate(file):
+            for j, char in enumerate(line.strip()):
+                image[i, j] = cast(Value, char)
+
+    background = cast(Value, ".")
+    with LineSpinner() as spinner:
+        for _ in range(50):
+            # print(background)
+            image, background = enhance(image, background, algorithm)
+            spinner.next()
+
+    assert background == "."
+    return len([value for value in image.values() if value == "#"])
 
 
 if __name__ == "__main__":
