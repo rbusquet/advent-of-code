31c35aff95f80d51d53ce49728e843c93114bb8b
Ricardo Busquet <7198302+rbusquet@users.noreply.github.com>
2021-12-21 00:36:32 -0500

    day 18 + tests

--- 2021/18/day18.py
+++ 2021/18/day18.py
@@ -1,15 +1,118 @@
 from __future__ import annotations
-from typing import Union
-import more_itertools.more
-
-Number = tuple[Union["Number", int], Union["Number", int]]  # type: ignore[misc]
-
-
-def addition(left: Number, right: Number) -> Number:
-    return reduce((left, right))
+import re
+from more_itertools import last
+import math
+from pathlib import Path
+import functools
+from itertools import combinations
 
 
-def reduce(number: Number, level: int = 0) -> Number | int:
-    if level == 4:
-        match
-    left, right = number
+re_single_number = re.compile(r"\d+")
+re_regular_number = re.compile(r"\[(\d+),(\d+)\]")
+
+
+def add(a: str, b: str) -> str:
+    return reduce(f"[{a},{b}]")
+
+
+def _count_levels(substr: str) -> int:
+    brackets = {"[": 1, "]": -1}
+    level = 0
+    for char in substr:
+        level += brackets.get(char, 0)
+    return level
+
+
+def replace_first_number(substr: str, number: int) -> str:
+    if found := re_single_number.search(substr):
+        value = int(found.group())
+        substr = re_single_number.sub(f"{value + number}", substr, 1)
+    return substr
+
+
+def replace_last_number(substr: str, number: int) -> str:
+    if found := last(re_single_number.finditer(substr), None):
+        value = int(found.group())
+        left = substr[0 : found.start()]
+        substr = re_single_number.sub(f"{value + number}", substr[found.start() :], 1)
+        substr = f"{left}{substr}"
+    return substr
+
+
+def explode(a: str) -> str:
+    for found in re_regular_number.finditer(a):
+        left = a[0 : found.start()]
+        right = a[found.end() :]
+        levels = _count_levels(left)
+        if levels >= 4:
+            x, y = map(int, found.groups())
+
+            right = replace_first_number(right, y)
+            left = replace_last_number(left, x)
+            return f"{left}0{right}"
+    return a
+
+
+def split_number(a: str) -> str:
+    for match in re_single_number.finditer(a):
+        value = int(match.group())
+        if value >= 10:
+            left = a[0 : match.start()]
+            right = a[match.end() :]
+            x, y = math.floor(value / 2), math.ceil(value / 2)
+            return f"{left}[{int(x)},{int(y)}]{right}"
+    return a
+
+
+def reduce(a: str) -> str:
+    while True:
+        exploded = explode(a)
+        if a != exploded:
+            a = exploded
+            continue
+        split = split_number(a)
+        if a != split:
+            a = split
+            continue
+        break
+    return a
+
+
+def _magnitude(x: int, y: int) -> int:
+    return 3 * x + 2 * y
+
+
+def magnitude(a: str) -> int:
+    while True:
+        if full_match := re_regular_number.fullmatch(a):
+            x, y = map(int, full_match.groups())
+            return _magnitude(x, y)
+        for found in re_regular_number.finditer(a):
+            x, y = map(int, found.groups())
+            left = a[0 : found.start()]
+            right = a[found.end() :]
+            a = f"{left}{_magnitude(x, y)}{right}"
+            break
+
+
+def part_1() -> int:
+    with open(Path(__file__).parent / "input.txt") as file:
+        numbers = map(lambda a: a.strip(), file.readlines())
+    result = functools.reduce(add, numbers)
+    return magnitude(result)
+
+
+def part_2() -> int:
+    with open(Path(__file__).parent / "input.txt") as file:
+        numbers = map(lambda a: a.strip(), file.readlines())
+    magnitudes = []
+
+    for x, y in combinations(numbers, 2):
+        magnitudes.append(magnitude(add(x, y)))
+        magnitudes.append(magnitude(add(y, x)))
+    return max(magnitudes)
+
+
+if __name__ == "__main__":
+    print(part_1())
+    print(part_2())
--- 2021/18/test_day18.py
+++ 2021/18/test_day18.py
@@ -1,12 +1,41 @@
-from day18 import addition
+from day18 import add, magnitude
+import functools
 
 
-def test_addition() -> None:
-    assert addition((1, 2), ((3, 4), 5)) == ((1, 2), ((3, 4), 5))
+def test_add() -> None:
+    assert add("[1,2]", "[[3,4],5]") == "[[1,2],[[3,4],5]]"
 
 
 def test_complex_addition() -> None:
-    left = ((((4, 3), 4), 4), (7, ((8, 4), 9)))
-    right = (1, 1)
-    expected = ((((0, 7), 4), ((7, 8), (6, 0))), (8, 1))
-    assert addition(left, right) == expected
+    expected = "[[[[0,7],4],[[7,8],[6,0]]],[8,1]]"
+    assert expected == add("[[[[4,3],4],4],[7,[[8,4],9]]]", "[1,1]")
+
+
+def test_multi_addition() -> None:
+    numbers = """
+[[[0,[4,5]],[0,0]],[[[4,5],[2,6]],[9,5]]]
+[7,[[[3,7],[4,3]],[[6,3],[8,8]]]]
+[[2,[[0,8],[3,4]]],[[[6,7],1],[7,[1,6]]]]
+[[[[2,4],7],[6,[0,5]]],[[[6,8],[2,8]],[[2,1],[4,5]]]]
+[7,[5,[[3,8],[1,4]]]]
+[[2,[2,2]],[8,[8,1]]]
+[2,9]
+[1,[[[9,3],9],[[9,0],[0,7]]]]
+[[[5,[7,4]],7],1]
+[[[[4,2],2],6],[8,7]]
+""".strip().split(
+        "\n"
+    )
+
+    result = functools.reduce(add, numbers)
+    expected = "[[[[8,7],[7,7]],[[8,6],[7,7]]],[[[0,7],[6,6]],[8,7]]]"
+    assert result == expected
+
+
+def test_magnitude() -> None:
+    assert magnitude("[[1,2],[[3,4],5]]") == 143
+    assert magnitude("[[[[0,7],4],[[7,8],[6,0]]],[8,1]]") == 1384
+    assert magnitude("[[[[1,1],[2,2]],[3,3]],[4,4]]") == 445
+    assert magnitude("[[[[3,0],[5,3]],[4,4]],[5,5]]") == 791
+    assert magnitude("[[[[5,0],[7,4]],[5,5]],[6,6]]") == 1137
+    assert magnitude("[[[[8,7],[7,7]],[[8,6],[7,7]]],[[[0,7],[6,6]],[8,7]]]") == 3488
