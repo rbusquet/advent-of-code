43a785ec97671ac20fcdde9efe56cfa6002436fe
Ricardo Busquet <7198302+rbusquet@users.noreply.github.com>
2022-05-02 20:10:41 -0400

    day 9

--- 2015/09/day9.py
+++ 2015/09/day9.py
@@ -3,7 +3,6 @@ import re
 import sys
 import typing as t
 from collections import defaultdict
-from copy import deepcopy
 from heapq import heappop, heappush
 from pathlib import Path
 
@@ -18,14 +17,12 @@ class Queue(t.Generic[T]):
     """
 
     def __init__(self) -> None:
-        self.pq = list[tuple[float, int, T]]()  # list of entries arranged in a heap
-        self.entry_finder = dict[
-            T, tuple[float, int, T]
-        ]()  # mapping of items to entries
+        self.pq = list[tuple[int, int, T]]()  # list of entries arranged in a heap
+        self.entry_finder = dict[T, tuple[int, int, T]]()  # mapping of items to entries
         self.REMOVED = "<removed-item>"  # placeholder for a removed item
         self.counter = itertools.count()  # unique sequence count
 
-    def add_item(self, item: T, priority: float = 0.0) -> None:
+    def add_item(self, item: T, priority: int = 0) -> None:
         "Add a new item or update the priority of an existing item"
         if item in self.entry_finder:
             self.remove_item(item)
@@ -42,7 +39,7 @@ class Queue(t.Generic[T]):
     def pop_item(self) -> T | None:
         "Remove and return the lowest priority item. Return None if empty."
         while self.pq:
-            *_, item = heappop(self.pq)
+            priority, _, item = heappop(self.pq)
             if item is not self.REMOVED:
                 del self.entry_finder[item]
                 return item
@@ -50,51 +47,90 @@ class Queue(t.Generic[T]):
         return None
 
 
-def Dijkstra(Graph, source):
-    dist = defaultdict[str, int](lambda: sys.maxsize)
-    dist[source] = 0
-    queue = Queue[str]()
-    prev = {}
+Item = tuple[int, tuple[int, ...]]
+Distance = list[tuple[int, int]]
 
-    for v in Graph:
-        if v != source:
-            prev[v] = None
-        queue.add_item(v, dist[v])
 
-    while True:
-        u = queue.pop_item()
-        if u is None:
-            break
-        for v, edge in Graph[u]:
-            alt = dist[u] + edge
-            if alt < dist[v]:
-                dist[v] = alt
-                prev[v] = u
-                queue.add_item(v, alt)
-    return dist, prev
+def Dijkstra(V: int, distances: defaultdict[int, Distance]) -> defaultdict[Item, int]:
+    cost = defaultdict[Item, int](lambda: sys.maxsize)
+    queue = Queue[Item]()
+    for node in range(0, V):
+        item = node, (node,)
+        cost[item] = 0
+        queue.add_item(item, 0)
+
+    while front := queue.pop_item():
+        current, mask = front
+        for child, weight in distances[current]:
+            if child in mask:
+                continue
+            next_path = mask + (child,)
+            child_item = child, next_path
+            tentative_distance = cost[front] + weight
+            if cost[child_item] > tentative_distance:
+                queue.add_item(child_item, tentative_distance)
+                cost[child, next_path] = tentative_distance
+    return cost
+
+
+class CounterDict(t.Generic[T], defaultdict[T, int]):
+    def __init__(self) -> None:
+        counter = itertools.count()
+        return super().__init__(lambda: next(counter))
 
 
 def part_1() -> int:
     file = Path(__file__).parent / "input.txt"
     contents = file.read_text()
-    graph = defaultdict[str, list[str, float]](list)
-    sources = list[str]()
+    nodes = CounterDict[str]()
+
+    distances = defaultdict[int, Distance](list)
     for line in contents.splitlines():
         if (match := expression.match(line)) is None:
             continue
-        a, b, value = match.groups()
-        graph[a].append((b, float(value)))
-        sources.append(a)
-    for source in sources:
-        dist, prev = Dijkstra(deepcopy(graph), source)
-        print(dist, prev)
+        a, b, value = map(str, match.groups())
+        da = nodes[a]
+        db = nodes[b]
+        distances[da].append((db, int(value)))
+        distances[db].append((da, int(value)))
+
+    cost = Dijkstra(len(nodes), distances)
+    answer = sys.maxsize
+
+    for n, path in cost.keys():
+        if len(path) != len(nodes):
+            continue
+        answer = min(answer, cost[n, path])
+        # print(path, "=>", cost[n, path])
+
+    return answer
 
 
 def part_2() -> int:
     file = Path(__file__).parent / "input.txt"
     contents = file.read_text()
+    nodes = CounterDict[str]()
+
+    distances = defaultdict[int, Distance](list)
     for line in contents.splitlines():
-        print(line)
+        if (match := expression.match(line)) is None:
+            continue
+        a, b, value = map(str, match.groups())
+        da = nodes[a]
+        db = nodes[b]
+        distances[da].append((db, int(value)))
+        distances[db].append((da, int(value)))
+
+    cost = Dijkstra(len(nodes), distances)
+    answer = 0
+
+    for n, path in cost.keys():
+        if len(path) != len(nodes):
+            continue
+        answer = max(answer, cost[n, path])
+        # print(path, "=>", cost[n, path])
+
+    return answer
 
 
 if __name__ == "__main__":
