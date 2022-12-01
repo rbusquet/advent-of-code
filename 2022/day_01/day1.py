9da0af213995952cb41ae26e93ba2d9b45c7259d
Ricardo Busquetttt <7198302+rbusquet@users.noreply.github.com>
2022-12-01 08:29:14 -0500

    day 1, 2022

--- 2022/day_01/day1.py
+++ 2022/day_01/day1.py
@@ -1,14 +1,31 @@
+from heapq import heappush, nlargest
 from pathlib import Path
 
 
 def part_1() -> int:
-    with open(Path(__file__).parent / "input.txt") as file:  # noqa: F841
-        pass
+    with open(Path(__file__).parent / "input.txt") as file:
+        elf = []
+        elves = []
+        for line in file:
+            if line.strip():
+                elf.append(int(line))
+            else:
+                elves.append(elf)
+                elf = []
+        return sum(max(elves, key=lambda e: sum(e)))
 
 
 def part_2() -> int:
-    with open(Path(__file__).parent / "input.txt") as file:  # noqa: F841
-        pass
+    with open(Path(__file__).parent / "input.txt") as file:
+        elf = []
+        elves = list[int]()
+        for line in file:
+            if line.strip():
+                elf.append(int(line))
+            else:
+                heappush(elves, sum(elf))
+                elf = []
+        return sum(nlargest(3, elves))
 
 
 if __name__ == "__main__":
--- download_inputs.py
+++ download_inputs.py
@@ -1,3 +1,4 @@
+import datetime
 import os
 from pathlib import Path
 
@@ -6,12 +7,15 @@ import requests
 session_cookie = os.environ["SESSION_COOKIE"]
 year = os.environ.get("YEAR") or 2019
 
+today = datetime.date.today()
 
 for i in range(1, 26):
+    if today.day > i:
+        break
     url = f"https://adventofcode.com/{year}/day/{i}/input"
     with requests.get(
         url, headers={"Cookie": f"session={session_cookie}"}
     ) as request, open(
-        Path(__file__).parent / f"{year}" / f"day{i}" / "input.txt", "w"
+        Path(__file__).parent / f"{year}" / f"day_{i:0>2}" / "input.txt", "w"
     ) as file:
         file.write(request.text)
