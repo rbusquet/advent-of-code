cd8bd3f88e30587e76b60470a870df54f69509d2
Ricardo Busquet <7198302+rbusquet@users.noreply.github.com>
2022-12-04 01:33:16 -0500

    day 4

---
+++ aoc_2022/day4.py
@@ -0,0 +1,32 @@
+import argparse
+import re
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("infile", type=argparse.FileType("r"))
+
+    args = parser.parse_args()
+
+    fully_contains_count = 0
+    overlaps_count = 0
+
+    for line in args.infile:
+        if not line.strip():
+            break
+        a, b, c, d = [int(x) for x in re.split("[,-]", line.strip())]
+        left, right = [(a, b), (c, d)]
+        ordered = tuple(sorted([a, b, c, d]))
+
+        fully_contains = ordered in [
+            (left[0], right[0], right[1], left[1]),
+            (right[0], left[0], left[1], right[1]),
+        ]
+
+        no_overlaps = (
+            ordered == (left[0], left[1], right[0], right[1]) and left[1] != right[0]
+        ) or (ordered == (right[0], right[1], left[0], left[1]) and right[1] != left[0])
+        fully_contains_count += fully_contains
+        overlaps_count += not no_overlaps
+
+    print("total fully contained:", fully_contains_count)
+    print("total overlaps:", overlaps_count)
