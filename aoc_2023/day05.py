97d2ab5a638ca940ec9370256278e97adc5e7be3
Ricardo Busquet <7198302+rbusquet@users.noreply.github.com>
2023-12-08 09:02:00 -0500

    day 5

--- aoc_2023/day05.py
+++ aoc_2023/day05.py
@@ -1,7 +1,9 @@
+from __future__ import annotations
+
 import argparse
 import sys
 from dataclasses import dataclass
-from typing import TextIO
+from typing import Any, Iterable, TextIO
 
 
 @dataclass
@@ -31,8 +33,81 @@ def part_1(file: TextIO) -> int:
     return min(seeds)
 
 
-def part_2(file: TextIO) -> int:
-    return -1
+@dataclass
+class Range:
+    start: int
+    length: int
+    mapped: bool = False
+
+    @property
+    def end(self) -> int:
+        return self.start + self.length - 1
+
+    def __and__(self, other: Range) -> Range | None:
+        left = max(self.start, other.start)
+        right = min(self.end, other.end)
+
+        if right < left:
+            return None
+
+        length = right - left + 1
+        return Range(left, length)
+
+    @classmethod
+    def start_to_end(cls, start: int, end: int) -> Range:
+        length = end - start + 1
+        return Range(start, length)
+
+
+def parse_ranges(file: TextIO) -> list[Range]:
+    seeds = [int(seed) for seed in next(file).strip().split(":")[1].strip().split()]
+    return [Range(*range) for range in zip(seeds[::2], seeds[1::2])]
+
+
+def strip_lines(file: TextIO) -> Iterable[str]:
+    for line in file:
+        yield line.strip()
+
+
+def part_2(file: TextIO):
+    file.seek(0)
+    ranges = parse_ranges(file)
+
+    for line in strip_lines(file):
+        if not line:
+            for range in ranges:
+                range.mapped = False
+            continue
+        if "map" in line:
+            continue
+
+        destination, source_start, length = map(int, line.strip().split())
+        source = Range(source_start, length)
+
+        for range in ranges:
+            if range.mapped:
+                continue
+            intersect = range & source
+
+            if intersect is None:
+                continue
+
+            if range.start < intersect.start:
+                ranges.append(
+                    Range.start_to_end(intersect.start, max(intersect.end, range.end))
+                )
+                range.length = Range.start_to_end(
+                    range.start, intersect.start - 1
+                ).length
+            if range.end > intersect.end:
+                ranges.append(Range.start_to_end(intersect.end + 1, range.end))
+                range.length = intersect.length
+
+            if range.start == intersect.start and range.end == intersect.end:
+                range.start = destination + range.start - source.start
+                range.mapped = True
+
+    return min(r.start for r in ranges)
 
 
 if __name__ == "__main__":
